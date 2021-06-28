51d6e21b15ac68e5ac496591c58fb792d1389c6a
macutko <matusgallik008@gmail.com>
2021-06-28 15:57:19 +0200

    fix: weird bug

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,73 @@
 import * as http from 'http';
 import {config} from 'dotenv';
-import app from './app';
 import {logger} from './util/logger';
 import fs from 'fs';
 import * as https from 'https';
+import express from 'express';
+import * as bodyParser from 'body-parser';
+import helmet from 'helmet';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import {connectMongoose} from './models/db';
+import * as ratingController from './api/controllers';
 
-config();
+const serve = async () => {
+    config();
 
-logger.info(`Checkign the DB: ${process.env.CONNECTION_STRING}`);
-// Certificate
-if (!process.env.DEV) {
+    const app = express();
 
-    const privateKey = fs.readFileSync('/etc/letsencrypt/live/asterdigital.tech/privkey.pem', 'utf8');
-    const certificate = fs.readFileSync('/etc/letsencrypt/live/asterdigital.tech/cert.pem', 'utf8');
-    const ca = fs.readFileSync('/etc/letsencrypt/live/asterdigital.tech/chain.pem', 'utf8');
+    app.use(bodyParser.urlencoded({extended: false}));
+    app.use(bodyParser.json());
+    app.use(express.static(__dirname + '/static', {dotfiles: 'allow'}));
 
+    app.use(helmet());
+    app.use(cors({
+        credentials: true,
+        origin: ['http://localhost:3000', 'https://redbull-test-client-oueo4qu8k-macutko.vercel.app/'],
+        allowedHeaders: ['Content-Type', 'Authorization']
+    }));
 
-    const credentials = {
-        key: privateKey,
-        cert: certificate,
-        ca: ca
-    };
-    const httpsServer = https.createServer(credentials, app);
-
-    httpsServer.listen(443, () => {
-        logger.info('HTTPS Server running on port 443');
+    app.use(cookieParser());
+    app.use((req, res) => {
+        res.send('Hello there !');
     });
-}
 
-const httpServer = http.createServer(app);
+    //init db
+    await connectMongoose();
 
 
-httpServer.listen(80, () => {
-    logger.info('HTTP Server running on port 80');
-});
+    // api routes
+    app.use('/rating', ratingController.default);
+
+
+    logger.info(`Checkign the DB: ${process.env.CONNECTION_STRING}`);
+    // Certificate
+    if (!process.env.DEV) {
+
+        const privateKey = fs.readFileSync('/etc/letsencrypt/live/asterdigital.tech/privkey.pem', 'utf8');
+        const certificate = fs.readFileSync('/etc/letsencrypt/live/asterdigital.tech/cert.pem', 'utf8');
+        const ca = fs.readFileSync('/etc/letsencrypt/live/asterdigital.tech/chain.pem', 'utf8');
+
+
+        const credentials = {
+            key: privateKey,
+            cert: certificate,
+            ca: ca
+        };
+        const httpsServer = https.createServer(credentials, app);
+
+        httpsServer.listen(443, () => {
+            logger.info('HTTPS Server running on port 443');
+        });
+    }
+    const httpServer = http.createServer(app);
+
+
+    httpServer.listen(8080, () => {
+        logger.info('HTTP Server running on port 80');
+    });
+
+
+};
+
+serve().then(() => logger.info('running')).catch(e => logger.error(e));
